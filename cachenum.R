ae7ca609a973e751efa75341591571c32bf71476
Ibrahim <ialzuhairi@gmail.com>
2018-01-25 21:51:45 +0300

    Assignment submission

--- cachenum.R
+++ cachenum.R
@@ -3,32 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix2 <- function(x = numeric()) {
   m <- NULL
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
   get <- function() x
-  setinverse <- function(inv) m <<- solve(inv)
-  getinverse <- function() m
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
   list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
+cacheSolve2 <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
+  m <- mean(data, ...)
+  x$setmean(m)
   m
 }
